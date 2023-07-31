2ede1acc40b6f3a808457a955e404aaee1b6b9f7
AhmedKamal1911 <khmedamal@gmail.com>
2023-07-31 05:08:09 +0300

    fix all problems

--- js/master.js
+++ js/master.js
@@ -119,3 +119,83 @@ exitBtn.addEventListener("click", (e) => {
   // Remove this line of code 120 + remove pages folder
   // location.assign("pages/test.html"); // i don't need to say ./pages/test.html because im changing the url only
 });
+
+// skills progress
+
+let skillsSection = document.querySelector(".skills");
+
+let skillsProgress = document.querySelectorAll(".skill-progress span");
+window.onscroll = function () {
+  // Skills Offset Top
+  let skillsOffsetTop = skillsSection.offsetTop;
+
+  // OuterHeight
+  let skillsOuterHeight = skillsSection.offsetHeight;
+
+  // Window Height
+  let windowHeight = this.innerHeight;
+
+  // Window ScrollTop
+  let windowScrollTop = this.scrollY;
+
+  if (windowScrollTop >= skillsOffsetTop + skillsOuterHeight - windowHeight) {
+    skillsProgress.forEach((span) => {
+      span.style.width = span.dataset.progress;
+    });
+  }
+
+  let imagesBox = document.querySelector(".images-box");
+
+  // Skills Offset Top
+  let imgsOffsetTop = imagesBox.offsetTop;
+
+  // OuterHeight
+  let imgsOuterHeight = imagesBox.offsetHeight;
+
+  if (windowScrollTop >= imgsOffsetTop + imgsOuterHeight - windowHeight) {
+    imagesBox.style.cssText = "transform: scale(1)";
+  }
+};
+// gallary Section
+
+// transform: translateX(-200%);
+let allImages = document.querySelectorAll(".images-box img");
+
+// on click make overlay
+allImages.forEach((img) => {
+  img.addEventListener("click", (e) => {
+    // create overlay div
+    let overlay = document.createElement("div");
+    overlay.classList.add("pop-up-overlay");
+    document.body.appendChild(overlay);
+    // create pop-up
+    let imgPopUp = document.createElement("div");
+    imgPopUp.classList.add("img-pop-up");
+    document.body.appendChild(imgPopUp);
+    if (img.alt !== null) {
+      // Create img title
+      let imgTitle = document.createElement("h2");
+      let imgTitleText = document.createTextNode(img.alt);
+      imgTitle.appendChild(imgTitleText);
+      imgPopUp.appendChild(imgTitle);
+    }
+    // Create popUP Img
+    let popUpImg = document.createElement("img");
+    popUpImg.src = img.src;
+    imgPopUp.appendChild(popUpImg);
+    // Create Close Btn
+    let closeBtn = document.createElement("i");
+    closeBtn.classList.add("close-btn");
+    closeBtn.innerHTML = "X";
+    imgPopUp.appendChild(closeBtn);
+    // Remove pop-Up
+    document.addEventListener("click", (e) => {
+      if (e.target.className == "close-btn") {
+        // Remove popUp
+        imgPopUp.remove();
+        // Remove Overlay
+        overlay.remove();
+      }
+    });
+  });
+});
